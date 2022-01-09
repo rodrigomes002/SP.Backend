d5b05d138661145db265c0ff58e0228086ae17cd
Rodrigo Gomes Calixto da Silva <rodrigo.gomes@gmail.com>
2022-01-09 18:46:35 -0300

    api

---
+++ SP.API/Controllers/ApiProcessos.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SP.Application.Processos;
+using SP.Application.Processos.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace SP.API.Controllers
+{
+    [Route("api/processos")]
+    [ApiController]
+    public class ApiProcessos : ControllerBase
+    {
+        private readonly ProcessoService _service;
+        private readonly ILogger<ApiProcessos> _logger;
+
+
+        public ApiProcessos(ProcessoService service, ILogger<ApiProcessos> logger)
+        {
+            this._logger = logger;
+            this._service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProcessoDTO>> Listar()
+        {
+            try
+            {
+                _logger.LogInformation("Recebendo requisição de listar todos os processos.");
+                var resultado = this._service.ListarTodos();
+                _logger.LogInformation("Retornando requisição de listar todos os processos.");
+                return Ok(resultado);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Falha na requisição: " + e.Message);
+                throw e;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ProcessoDTO> Listar(long id)
+        {
+            try
+            {
+                _logger.LogInformation("Recebendo requisição de listar um processo.");
+                var resultado = this._service.ListarPorId(id);
+                _logger.LogInformation("Retornando requisição de listar um processo.");
+                return Ok(resultado);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Falha na requisição :" + e.Message);
+                throw e;
+            }
+        }
+
+        [HttpPost("cadastrar")]
+        public ActionResult Cadastrar([FromBody] ProcessoDTO requisicao)
+        {
+            try
+            {
+                _logger.LogInformation("Recebendo requisição para cadastrar um processo.");
+                this._service.Cadastrar(requisicao);
+                _logger.LogInformation("Retornando requisição para cadastrar um processo.");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Falha na requisição: " + e.Message);
+                throw e;
+            }
+        }
+
+        [HttpPut("atualizar")]
+        public ActionResult Atualizar([FromBody] ProcessoDTO requisicao)
+        {
+            try
+            {
+                _logger.LogInformation("Recebendo requisição para cadastrar um processo.");
+                this._service.Atualizar(requisicao);
+                _logger.LogInformation("Retornando requisição para cadastrar um processo.");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Falha na requisição: " + e.Message);
+                throw e;
+            }
+        }
+    }
+}
---
+++ SP.API/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SP.Application.Processos;
+using SP.Domain;
+using SP.Domain.Processos.Interface;
+using SP.Infrastructure;
+using SP.Infrastructure.Context;
+
+namespace SP.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ProcessoService>();
+            services.AddScoped<UnitOfWork, EFUnitOfWork>();
+            services.AddScoped<ProcessoRepository, EFProcessoRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
