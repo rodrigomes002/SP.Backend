a974aa1097808356190124cc66cabd253b9463a9
Rodrigo Gomes Calixto da Silva <rodrigo.gomes@gmail.com>
2022-01-09 18:45:37 -0300

    camada application

---
+++ SP.Application/Processos/DTO/ProcessoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SP.Application.Processos.DTO
+{
+    public class ProcessoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }        
+    }
+}
---
+++ SP.Application/Processos/ProcessoService.cs
@@ -0,0 +1,59 @@
+﻿using SP.Application.Processos.DTO;
+using SP.Domain;
+using SP.Domain.Processos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SP.Application.Processos
+{
+    public class ProcessoService
+    {
+        private readonly UnitOfWork unitOfWork;
+        public ProcessoService(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Atualizar(ProcessoDTO requisicao)
+        {
+            var processo = this.unitOfWork.ProcessoRepository.ListarPorId(requisicao.Id);
+
+            if (processo is null)
+            {
+                throw new Exception("Processo não encontrado.");
+            }
+
+            processo.Nome = requisicao.Nome;
+            this.unitOfWork.ProcessoRepository.Atualizar(processo);
+            this.unitOfWork.Commit();
+        }
+
+        public void Cadastrar(ProcessoDTO requisicao)
+        {
+            var processo = new Processo(requisicao.Nome);
+            this.unitOfWork.ProcessoRepository.Cadastrar(processo);
+            this.unitOfWork.Commit();
+        }
+
+        public ProcessoDTO ListarPorId(long id)
+        {
+            var processo = this.unitOfWork.ProcessoRepository.ListarPorId(id);
+
+            if (processo is null)
+            {
+                throw new Exception("Processo não encontrado.");
+            }
+
+            var dto = new ProcessoDTO() { Id = processo.Id, Nome = processo.Nome };
+            return dto;
+        }
+
+        public IEnumerable<ProcessoDTO> ListarTodos()
+        {
+            var processos = this.unitOfWork.ProcessoRepository.ListarTodos();
+            var resultado = processos.Select(processo => new ProcessoDTO { Id = processo.Id, Nome = processo.Nome });
+            return resultado;
+        }
+    }
+}
